e220519ad50ba8babd4fc795f323e051f17a43c6
Davi Ribeiro <111585899+daviribeiroads@users.noreply.github.com>
2022-11-17 19:14:50 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./componets/NotificationButton"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
   
 }
